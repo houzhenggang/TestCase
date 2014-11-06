d046257dc460325021be11862c541fd5215ded31
Tang Min <doctang@163.com>
2014-11-06 20:16:49 +0800

    Commit 3

--- TestCase/test_mini_case/start.py
+++ TestCase/test_mini_case/start.py
@@ -28,6 +28,37 @@ class DumpsysMeminfoThread(threading.Thread):
                     shell=True, stdout=output, stderr=output).wait()
         output.close()
 
+        output = open(os.path.join(self.outdir, 'meminfo.txt'), 'r')
+        list = []
+        data = {}
+        for line in output.readlines():
+            if line.strip().startswith('Native'):
+                m = re.match('Native[ Heap]+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)', line.strip())
+                if m:
+                    data['native_heap_size'] = m.groups()[4]
+                    data['native_heap_alloc'] = m.groups()[5]
+                    data['native_heap_free'] = m.groups()[6]
+            elif line.strip().startswith('Dalvik'):
+                m = re.match('Dalvik[ Heap]+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)', line.strip())
+                if m:
+                    data['dalvik_heap_size'] = m.groups()[4]
+                    data['dalvik_heap_alloc'] = m.groups()[5]
+                    data['dalvik_heap_free'] = m.groups()[6]
+            if len(data) == 6:
+                list.append(data.copy())
+                data.clear()
+        output.close()
+
+        report = open(os.path.join(self.outdir, 'meminfo.csv'), 'w')
+        report.write(codecs.BOM_UTF8)
+        report.write(','.join(('Native Heap Size', 'Native Heap Alloc', 'Native Heap Free',
+                'Dalvik Heap Size', 'Dalvik Heap Alloc', 'Dalvik Heap Free')) + '\n')
+        if len(list) > 0:
+            for item in list:
+                report.write(','.join((item['native_heap_size'], item['native_heap_alloc'], item['native_heap_free'],
+                        item['dalvik_heap_size'], item['dalvik_heap_alloc'], item['dalvik_heap_free'])) + '\n')
+        report.close()
+
     def stop(self):
         self.loop = False
 
@@ -48,6 +79,22 @@ class DumpsysGfxinfoThread(threading.Thread):
                     shell=True, stdout=output, stderr=output).wait()
         output.close()
 
+        output = open(os.path.join(self.outdir, 'gfxinfo.txt'), 'r')
+        list = []
+        for line in output.readlines():
+            m = re.match('(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)', line.strip())
+            if m:
+                list.append((m.groups()[0], m.groups()[1], m.groups()[2]))
+        output.close()
+
+        report = open(os.path.join(self.outdir, 'gfxinfo.csv'), 'w')
+        report.write(codecs.BOM_UTF8)
+        report.write(','.join(('Draw', 'Process', 'Execute')) + '\n')
+        if len(list) > 0:
+            for item in list:
+                report.write(','.join(item) + '\n')
+        report.close()
+
     def stop(self):
         self.loop = False
 
@@ -61,17 +108,18 @@ class DumpsysCpuinfoThread(threading.Thread):
         self.loop = True
 
     def run(self):
-        output = open(os.path.join(self.outdir, 'cpuinfo.csv'), 'w')
-        output.write(codecs.BOM_UTF8)
-        output.write('当前频率,当前温度\n')
-        output.flush()
+        report = open(os.path.join(self.outdir if self.package else workout, 'cpuinfo.csv'), 'w')
+        report.write(codecs.BOM_UTF8)
+        report.write('当前频率,当前温度\n')
+        report.flush()
         while self.loop:
             time.sleep(self.interval)
             curfreq = os.popen('adb shell cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq').readline().strip()
             curtemp = os.popen('adb shell cat /sys/class/thermal/thermal_zone0/temp').readline().strip()
-            output.write('{0},{1}\n'.format(curfreq, curtemp))
-            output.flush()
-        output.close()
+            if curfreq and curtemp:
+                report.write('{0},{1}\n'.format(curfreq, curtemp))
+                report.flush()
+        report.close()
 
     def stop(self):
         self.loop = False
@@ -113,31 +161,25 @@ def getAppsInfo(package, title, activity):
             title, size, t, min(t[1:]), max(t[1:]), round(float(sum(t[1:])) / (len(t) - 1), 1)))
     output.close()
 
-def monkey(package):
-    outdir = os.path.join(workout, 'monkey-{0}'.format(package))
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
+def monkey(package=None):
+    if package:
+        outdir = os.path.join(workout, package)
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+    else:
+        outdir = workout
 
-    t1 = DumpsysMeminfoThread(package, 20, outdir)
-    t2 = DumpsysGfxinfoThread(package, 10, outdir)
-    t3 = DumpsysCpuinfoThread(package, 10, outdir)
-    t1.start()
-    t2.start()
-    t3.start()
-    monkey = open(os.path.join(outdir, 'monkey.txt'), 'w+')
-    subprocess.Popen('adb shell monkey -p {0} -s 10 --throttle 100 --pct-anyevent 0 --ignore-timeouts --ignore-crashes -v 100000'.format(
-            package), shell=True, stdout=monkey, stderr=monkey).wait()
-    monkey.close()
-    t1.stop()
-    t2.stop()
-    t3.stop()
-    t1.join()
-    t2.join()
-    t3.join()
+    output = open(os.path.join(outdir, 'monkey.txt'), 'w+')
+    if package:
+        command = 'adb shell monkey -p {0} -s 10 --throttle 100 --ignore-timeouts --ignore-crashes -v 50000'.format(package)
+    else:
+        command = 'adb shell monkey -s 10 --throttle 100 --ignore-timeouts --ignore-crashes -v 500000'
+    subprocess.Popen(command, shell=True, stdout=output, stderr=output).wait()
+    output.close()
 
-    monkey = open(os.path.join(outdir, 'monkey.txt'), 'r')
+    output = open(os.path.join(outdir, 'monkey.txt'), 'r')
     data = {'count': 0, 'event': 0, 'crash': 0, 'anr': 0}
-    for line in monkey.readlines():
+    for line in output.readlines():
         if line.startswith(':Monkey:'):
             m = re.match(':Monkey: seed=(\d+) count=(\d+)', line)
             data['count'] = m.groups()[1]
@@ -151,7 +193,7 @@ def monkey(package):
             data['crash'] = data['crash'] + 1
         elif line.startswith('// NOT RESPONDING:'):
             data['anr'] = data['anr'] + 1
-    monkey.close()
+    output.close()
 
     report = open(os.path.join(outdir, 'monkey.csv'), 'w')
     report.write(codecs.BOM_UTF8)
@@ -159,52 +201,11 @@ def monkey(package):
     report.write('{0},{1},{2},{3}\n'.format(data['count'], data['event'], data['crash'], data['anr']))
     report.close()
 
-    meminfo = open(os.path.join(outdir, 'meminfo.txt'), 'r')
-    list = []
-    data = {}
-    for line in meminfo.readlines():
-        if line.strip().startswith('Native'):
-            m = re.match('Native[ Heap]+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)', line.strip())
-            if m:
-                data['native_heap_size'] = m.groups()[4]
-                data['native_heap_alloc'] = m.groups()[5]
-                data['native_heap_free'] = m.groups()[6]
-        elif line.strip().startswith('Dalvik'):
-            m = re.match('Dalvik[ Heap]+\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)', line.strip())
-            if m:
-                data['dalvik_heap_size'] = m.groups()[4]
-                data['dalvik_heap_alloc'] = m.groups()[5]
-                data['dalvik_heap_free'] = m.groups()[6]
-        if len(data) == 6:
-            list.append(data.copy())
-            data.clear()
-    meminfo.close()
-
-    report = open(os.path.join(outdir, 'meminfo.csv'), 'w')
-    report.write(codecs.BOM_UTF8)
-    report.write(','.join(('Native Heap Size', 'Native Heap Alloc', 'Native Heap Free',
-            'Dalvik Heap Size', 'Dalvik Heap Alloc', 'Dalvik Heap Free')) + '\n')
-    if len(list) > 0:
-        for item in list:
-            report.write(','.join((item['native_heap_size'], item['native_heap_alloc'], item['native_heap_free'],
-                    item['dalvik_heap_size'], item['dalvik_heap_alloc'], item['dalvik_heap_free'])) + '\n')
-    report.close()
-
-    gfxinfo = open(os.path.join(outdir, 'gfxinfo.txt'), 'r')
-    list = []
-    for line in gfxinfo.readlines():
-        m = re.match('(\d+\.\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)', line.strip())
-        if m:
-            list.append((m.groups()[0], m.groups()[1], m.groups()[2]))
-    gfxinfo.close()
-
-    report = open(os.path.join(outdir, 'gfxinfo.csv'), 'w')
-    report.write(codecs.BOM_UTF8)
-    report.write(','.join(('Draw', 'Process', 'Execute')) + '\n')
-    if len(list) > 0:
-        for item in list:
-            report.write(','.join(item) + '\n')
-    report.close()
+    while True:
+        os.system('adb wait-for-device')
+        if not os.popen('adb shell /data/local/tmp/busybox pidof com.android.commands.monkey').readline().strip():
+            break
+        time.sleep(3)
 
 def reboot():
     os.system('adb reboot')
@@ -266,43 +267,105 @@ def install():
     report.close()
 
 def main():
-    shutil.rmtree(workout, ignore_errors=True)
-    if not os.path.exists(workout):
-        os.mkdir(workout)
+    state = os.popen('adb get-state').readline().strip()
+    if state == 'device':
+        print('Operation type choices are:')
+        print('    1. monkey')
+        print('    2. single')
+        print('    3. manual')
+        try:
+            operation = input('\nWhich would you like? [1] ')
+        except SyntaxError:
+            operation = 1
+        except NameError:
+            sys.exit(2)
 
-    os.popen('adb install -r \"{0}\"'.format(os.path.join(workdir, 'TestCommon.apk'))).readlines()
-    os.popen('adb install -r \"{0}\"'.format(os.path.join(workdir, 'SettingsTest.apk'))).readlines()
+        global workout
+        if not os.path.exists(workout):
+            os.mkdir(workout)
+        if operation == 1:
+            workout = os.path.join(workout, 'monkey')
+        elif operation == 2:
+            workout = os.path.join(workout, 'single')
+        elif operation == 3:
+            workout = os.path.join(workout, 'manual')
+        else:
+            sys.exit(2)
+        if not os.path.exists(workout):
+            os.mkdir(workout)
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'p:')
-    except getopt.GetoptError:
-        sys.exit(2)
+        model = os.popen('adb shell getprop ro.product.model').readline().strip()
+        workout = os.path.join(workout, model)
+        shutil.rmtree(workout, ignore_errors=True)
+        if not os.path.exists(workout):
+            os.mkdir(workout)
 
-    packages = []
-    for option, value in opts:
-        if option == '-p':
-            packages.append(value)
+        os.popen('adb push \"{0}\" /data/local/tmp'.format(os.path.join(workdir, 'busybox'))).readlines()
+        os.popen('adb shell chmod 755 /data/local/tmp/busybox').readlines()
 
-    if len(packages) == 0:
+        os.popen('adb install -r \"{0}\"'.format(os.path.join(workdir, 'TestCommon.apk'))).readlines()
+        os.popen('adb shell am startservice --user 0 -W -n com.ztemt.test.common/.PackageService --es command getLauncherList').readlines()
+        time.sleep(3)
+        jobj = eval(os.popen('adb shell cat /data/data/com.ztemt.test.common/files/launcher').readline())
+
+        os.popen('adb install -r \"{0}\"'.format(os.path.join(workdir, 'SettingsTest.apk'))).readlines()
+        os.popen('adb shell am instrument -w -e class com.android.settings.test.DevelopmentSettingsTestCase#testTrackFrameTimeIsDumpsysGfxinfo '
+                'com.android.settings.test/com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner').readlines()
+
         packages = [line[8:].strip() for line in os.popen('adb shell pm list package -s').readlines()]
-
-    os.popen('adb shell am startservice --user 0 -W -n com.ztemt.test.common/.PackageService --es command getLauncherList').readlines()
-    time.sleep(3)
-    jobj = eval(os.popen('adb shell cat /data/data/com.ztemt.test.common/files/launcher').readline())
-    os.popen('adb shell am instrument -w -e class com.android.settings.test.DevelopmentSettingsTestCase#testTrackFrameTimeIsDumpsysGfxinfo com.android.settings.test/com.google.android.apps.common.testing.testrunner.GoogleInstrumentationTestRunner').readlines()
-
-    for package in packages:
-        if package in jobj:
+        packages = [package for package in packages if package in jobj]
+        for package in packages:
             for item in jobj[package]:
                 getAppsInfo(package, item['title'], item['activity'])
-            monkey(package)
+        if operation == 1:
+            threads = []
+            for package in packages:
+                outdir = os.path.join(workout, package)
+                if not os.path.exists(outdir):
+                    os.mkdir(outdir)
+                t1 = DumpsysMeminfoThread(package, 20, outdir)
+                t2 = DumpsysGfxinfoThread(package, 10, outdir)
+                threads.append(t1)
+                threads.append(t2)
+                t1.start()
+                t2.start()
+            t3 = DumpsysCpuinfoThread(None, 10, outdir)
+            t3.start()
+            monkey()
+            for t in threads:
+                t.stop()
+            for t in threads:
+                t.join()
+            t3.stop()
+            t3.join()
+        elif operation == 2:
+            for package in packages:
+                outdir = os.path.join(workout, package)
+                if not os.path.exists(outdir):
+                    os.mkdir(outdir)
+                t1 = DumpsysMeminfoThread(package, 20, outdir)
+                t2 = DumpsysGfxinfoThread(package, 10, outdir)
+                t3 = DumpsysCpuinfoThread(package, 10, outdir)
+                t1.start()
+                t2.start()
+                t3.start()
+                monkey(package)
+                t1.stop()
+                t2.stop()
+                t3.stop()
+                t1.join()
+                t2.join()
+                t3.join()
+        elif operation == 3:
+            pass
+        install()
+        reboot()
 
-    install()
-
-    reboot()
-
-    os.popen('adb uninstall com.ztemt.test.common').readlines()
-    os.popen('adb uninstall com.android.settings.test').readlines()
+        os.popen('adb shell rm /data/local/tmp/busybox').readlines()
+        os.popen('adb uninstall com.ztemt.test.common').readlines()
+        os.popen('adb uninstall com.android.settings.test').readlines()
+    else:
+        print('Please connect your device')
 
 workdir = os.path.dirname(os.path.realpath(sys.argv[0]))
 workout = os.path.join(workdir, 'out')
